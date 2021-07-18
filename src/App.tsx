f8f6f1ba71a747628b36962dea0a5055945735a9
Mohammad Ali Jafarian <m.ali.jafarian71@gmail.com>
2021-07-18 17:40:44 +0430

    adds SearchPage to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { CssBaseline } from "@material-ui/core";
 import { FunctionComponent } from "react";
+import SearchPage from "./pages/SearchPage";
 
 const App: FunctionComponent = (props) => {
-  return <div></div>;
+  return (
+    <>
+      <CssBaseline />
+      <SearchPage />
+    </>
+  );
 };
 
 export default App;
