edddd8c06033111b86ec48f79d601d85f7e8d566
Mohammad Ali Jafarian <m.ali.jafarian71@gmail.com>
2021-07-20 03:32:42 +0430

    update integration tests to match requirements

--- cypress/integration/searchpage_spec.ts
+++ cypress/integration/searchpage_spec.ts
@@ -2,7 +2,7 @@ describe("Visiting the website", () => {
   it("Display right contents when website is visited", () => {
     cy.visit("http://localhost:3000");
     cy.get("[data-testid='searchInput']").should("have.value", "");
-    cy.get("[data-testid='genderInput']").should("have.value", "unisex");
+    cy.get("[data-testid='genderInput']").should("have.value", "");
     cy.get("[data-testid='onSaleSwitch']")
       .get("input")
       .should("not.have.attr", "checked");
@@ -24,30 +24,35 @@ describe("Searching for products", () => {
   it("searchbox products should be related to query", () => {
     cy.get("[data-testid='searchProductGrid']").should(
       "have.length.at.least",
-      1
+      1,
+      "article"
     );
     cy.get("[data-testid='searchProductGrid']").should(
       "have.length.at.most",
-      100
+      100,
+      "article"
     );
     cy.get("[data-testid='searchProductGrid']").should(
       "not.contain.text",
       "Start searching for products by typing in the search box"
     );
-    cy.get("[data-testid='searchProductGrid']").children().contains("slim");
+    cy.get("[data-testid='productTitle']").contains("slim", {
+      matchCase: false,
+    });
   });
 
   it("when user change gender it should show only related products for gender", () => {
     // choosing gender
     cy.get("[data-testid='genderInput']").select("female");
-    cy.get("[data-testid='searchProductGrid']")
-      .children()
-      .get("[data-testid='productGender']")
-      .should("not.contain.text", "male");
-    cy.get("[data-testid='searchProductGrid']")
-      .children()
-      .get("[data-testid='productGender']")
-      .should("not.contain.text", "unisex");
+    cy.wait(500);
+    cy.get("[data-testid='productGender']").should(
+      "not.contain.text",
+      "unisex"
+    );
+    cy.get("[data-testid='productGender']").should(
+      "not.contain.contain",
+      /male/
+    );
   });
 
   it("when user change onlyOnSaleSwitch it should show on sale products", () => {
@@ -57,19 +62,35 @@ describe("Searching for products", () => {
       .children()
       .get("[data-testid='saleLabel']");
   });
-});
 
-describe("displaying searched products", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-    // typing in search box
-    cy.get("[data-testid='searchInput']").type("slim");
-    // because input change event is debounced for 500 ms
-    cy.wait(500);
-  });
   it("when clicking on more button should show extera images in product cells", () => {
-    const firstCell = cy.get("[data-testid='searchProductGrid']").first();
-    firstCell.get("[data-testid='moreImageButton']").click();
-    firstCell.get("img").should("have.length.greaterThan", 1);
+    cy.get("[data-testid='moreImageButton']").first().click();
+    cy.get("[data-testid='searchProductGrid']").within(() => {
+      cy.get("article")
+        .first()
+        .within(() => {
+          cy.get("img").should("have.length.greaterThan", 1);
+        });
+    });
+
+    cy.get("[data-testid='moreImageButton']").first().click();
+    cy.get("[data-testid='searchProductGrid']").within(() => {
+      cy.get("article")
+        .first()
+        .within(() => {
+          cy.get("img").should("have.length", 1);
+        });
+    });
+  });
+
+  it("scrolling should lazy load images", () => {
+    cy.get("article").last().scrollIntoView({});
+    cy.get("article")
+      .last()
+      .within(() => {
+        cy.get("img")
+          .invoke("attr", "src")
+          .should("not.eq", "/placeholder.png");
+      });
   });
 });
