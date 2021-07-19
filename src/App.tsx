706c9d7b525908256818e2f4a711a2677cdbb8b6
Mohammad Ali Jafarian <m.ali.jafarian71@gmail.com>
2021-07-19 19:25:52 +0430

    adds fetchProducts side effect to App component to load products when app starts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import { CssBaseline } from "@material-ui/core";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect } from "react";
+import { useAppDispatch } from "./hooks/redux";
+
 import SearchPage from "./pages/SearchPage";
+import { fetchProducts } from "./reducers/products";
 
 const App: FunctionComponent = (props) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const promise = dispatch(fetchProducts());
+    return () => {
+      promise.abort();
+    };
+  }, [dispatch]);
+
   return (
     <>
       <CssBaseline />
