1d72f1163050eda25c5e37033349be5c676d0531
Mohammad Ali Jafarian <m.ali.jafarian71@gmail.com>
2021-07-19 19:50:38 +0430

    fixes SearchProductGrid layout

--- src/pages/SearchPage/components/SearchProductGrid/styles.ts
+++ src/pages/SearchPage/components/SearchProductGrid/styles.ts
@@ -2,10 +2,21 @@ import { makeStyles } from "@material-ui/core";
 
 const useSearchProductGridStyles = makeStyles((theme) => ({
   container: {
-    display: "flex",
-    flexWrap: "wrap",
+    display: "grid",
+    gridGap: theme.spacing(1),
+    gridTemplateColumns: "1fr",
+    [theme.breakpoints.up("sm")]: {
+      gridTemplateColumns: "1fr 1fr",
+    },
+    [theme.breakpoints.up("md")]: {
+      gridTemplateColumns: "1fr 1fr 1fr",
+    },
+    [theme.breakpoints.up("lg")]: {
+      gridTemplateColumns: "1fr 1fr 1fr 1fr",
+    },
   },
   centered: {
+    display: "flex",
     alignItems: "center",
     justifyContent: "center",
   },
