227302bf7be203fd58bf62d3c55ec8ddc8a9c4f8
Mohammad Ali Jafarian <m.ali.jafarian71@gmail.com>
2021-07-20 23:13:01 +0430

    adds service worker for caching images and app bundle

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
@@ -14,7 +15,14 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+console.log(new URL("https://mosaic01.ztat.net/vgs/media/large/TF/32/1S/00/0N/11/TF321S000-N11@12.jpg").origin)
